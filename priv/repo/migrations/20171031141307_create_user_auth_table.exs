309b5f0aaa7aa702afec5ca5eb0861d8fdffc301
febil <febil.tvr@gmail.com>
2017-12-05 20:56:39 +0530

    migration error fix

--- priv/repo/migrations/20171031141307_create_user_auth_table.exs
+++ priv/repo/migrations/20171031141307_create_user_auth_table.exs
@@ -1,13 +1,18 @@
-defmodule CinemaApp.Repo.Migrations.CreateUserAuthTable do
+defmodule CinemaApp.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
     create table(:users) do
-      add :email, :string
       add :name, :string
+      add :username, :string
+      add :email, :string
       add :password_hash, :string
+
       timestamps()
     end
+
+    create unique_index(:users, [:username])
     create unique_index(:users, [:email])
+
   end
 end
